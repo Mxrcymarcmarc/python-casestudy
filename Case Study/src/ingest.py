3e946f8fb1ac8f384a46314fe182c49bb4dcaf55
Mxrcymarcmarc <lemuel.marc.celis@gmail.com>
2025-11-05 08:20:30 +0800

    Update ingest.py

--- Case Study/src/ingest.py
+++ Case Study/src/ingest.py
@@ -4,4 +4,10 @@ import csv
 def read_csv(file_path):
     students = []
 
-
+    with open(file_path, mode='r', newline='', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        
+        for row in reader:
+            student = {}
+            
+            student["student_id"] = row["student_id"].strip()
