032b957390a39dc477bad557c31ea5008766cb0e
Mxrcymarcmarc <lemuel.marc.celis@gmail.com>
2025-11-05 10:20:42 +0800

    Update transform.py

--- Case Study/src/transform.py
+++ Case Study/src/transform.py
@@ -1,1 +1,20 @@
 # computing the final grade, and letter grades
+import json
+import statistics
+
+def load_config(config_file="config.json"):
+    with open(config_file, "r") as file:
+        return json.load(file)
+    
+def quiz_average(quizzes):
+    valid_scores = [q for q in quizzes if q is not None]
+    
+    if not valid_scores:
+        return None
+    
+    return statistics.mean(valid_scores)
+
+def weights(student, weights):
+    quiz_average = student["quiz_average"]
+    midterm = student["midterm"]
+    final = student["final"]
